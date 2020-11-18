4d2007973cd96a922129e6526e74896ac29c8bad
Arief Gusti <arief@hellonimbly.com>
2020-11-18 07:45:42 +0700

    add products basic functions

--- src/main/java/com/ariefgusti/model/Products.java
+++ src/main/java/com/ariefgusti/model/Products.java
@@ -33,6 +33,55 @@ public class Products {
         this.sellerId = sellerId;
         this.dateCreated = dateCreated;
         this.dateUpdated = dateUpdated;
+    }
+    
+    public long getProductId() {
+		return productId;
+    }
+    
+    public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+    }
+    
+    public Integer getHargaSatuan() {
+		return hargaSatuan;
+	}
+
+	public void setHargaSatuan(Integer hargaSatuan) {
+		this.hargaSatuan = hargaSatuan;
+    }
+    
+    public long getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(long sellerId) {
+		this.sellerId = sellerId;
+    }
+
+    public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+    }
+
+    public Date getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(Date dateUpdated) {
+		this.dateUpdated = dateUpdated;
+    }
+
+    @Override
+    public String toString() {
+		return "Products [productId=" + productId + ", nama=" + nama + ", hargaSatuan=" + hargaSatuan + ", sellerId=" + sellerId + ", dateCreated=" + dateCreated + ", dateUpdated=" + dateUpdated + "]";
 	}
 
 }
