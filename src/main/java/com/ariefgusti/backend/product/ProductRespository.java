41b6e18aa9ba3e3fdc7bcf52edd5c3fec7271d51
Arief Gusti <arief@hellonimbly.com>
2020-11-18 13:47:05 +0700

    create service and API

--- src/main/java/com/ariefgusti/backend/product/ProductRespository.java
+++ src/main/java/com/ariefgusti/backend/product/ProductRespository.java
@@ -3,4 +3,5 @@ package com.ariefgusti.backend.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRespository extends JpaRepository<Product, Long> {
+    
 }
---
+++ src/main/java/com/ariefgusti/backend/product/ProductService.java
@@ -0,0 +1,29 @@
+package com.ariefgusti.backend.product;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRespository productRespository;
+
+    public List<Product> findAll() {
+        return productRespository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRespository.findById(id);
+    }
+
+    public Product save(Product stock) {
+        return productRespository.save(stock);
+    }
+
+    public void deleteById(Long id) {
+        productRespository.deleteById(id);
+    }
+}
