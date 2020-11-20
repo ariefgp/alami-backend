38ab3bc9bf4dcc6ba3a3953f4b3e13969576e227
Arief Gusti <arief@hellonimbly.com>
2020-11-20 18:05:54 +0700

    add seller controller

---
+++ src/main/java/com/ariefgusti/backend/seller/SellerController.java
@@ -0,0 +1,11 @@
+package com.ariefgusti.backend.seller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SellerController {
+    @GetMapping("/")
+    public String list(){
+        return "sellers";
+    }
+}
