e7c64e7fe98c7d82d9f474ceb721b1536a7ee686
Arief Gusti <arief@hellonimbly.com>
2020-11-20 16:04:08 +0700

    initiate seller table

--- src/main/java/com/ariefgusti/backend/seller/Seller.java
+++ src/main/java/com/ariefgusti/backend/seller/Seller.java
@@ -6,15 +6,16 @@ import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.math.BigDecimal;
+import javax.persistence.Table;
+
 import java.util.Date;
 
-@Entity
+@Entity(name = "Seller")
 @Data
+@Table(name = "seller")
 public class Seller {
 
     @Id
